8b8bcb6471e2f2f8bd1a7fd023922faf17ea7d2a
anyalopatiuc <lopatukann8@gmail.com>
2025-11-13 19:59:41 +0100

    gfm

---
+++ 07-Arrays/1,4.py
@@ -0,0 +1,11 @@
+arr = [2, 3, 7, 5, 4]
+
+print(arr)
+print('Number of elements', len(arr))
+print('First value', arr[0])
+print('Second value', arr[1])
+print('last value', arr[-1])
+print('Second value', len(arr)-1)
+print('sum', arr[0]+(len(arr)-1))
+print('midde', int(arr/2))
+print('separated', value,end="") # type: ignore
---
+++ 07-Arrays/1,5.py
@@ -0,0 +1,14 @@
+Array= [1,2,3,4,5]
+Array= [0,2,3,4,5]
+Array= [0,2,3,4,9]
+Array= [0,2,6,4,9]
+
+incr= (len(Array)-1) + 4
+print('by 4',  incr)
+
+sub= (Array[0])-1
+print('sub',sub)
+
+t=Array[len(Array)//2]
+mul= (t*2)
+print('mul',mul)
